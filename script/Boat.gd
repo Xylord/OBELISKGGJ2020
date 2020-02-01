b79028c8e880d095453be35bb76f56a09db3f2b8
unknown <xylord.khan@gmail.com>
2020-02-01 14:16:34 -0500

    Added lightning model and correcting moment

--- script/Boat.gd
+++ script/Boat.gd
@@ -11,6 +11,8 @@ var rotationThrust = .5;
 var max_velocity = 10.0;
 var  max_rotation = 2;
 
+var ocean;
+
 #health 
 export(float) var max_health = 100
 onready var health = max_health setget _set_health
@@ -21,10 +23,12 @@ signal killed()
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	thrust = Vector3()
+	
+	ocean = get_parent().get_node('Ocean')
 	pass # Replace with function body.
 
 		
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_just_pressed("press_k"):
 		print("Ouch")
 		damage(10)
@@ -32,13 +36,29 @@ func _physics_process(delta):
 	var ocean = get_parent().get_node('Ocean')
 	
 	var translation = ocean.get_displace(Vector2(global_transform.origin.x, global_transform.origin.z))
+	var xP = global_transform.origin.x
+	var zP = global_transform.origin.z
+	var d1 = ocean.get_displace(Vector2(xP, zP))
+	var d2 = ocean.get_displace(Vector2(xP+0.01, zP))
+	var d3 = ocean.get_displace(Vector2(xP, zP+0.01))
 	var buoyancyForce = translation.y - global_transform.origin.y
 	buoyancyForce = clamp(buoyancyForce, 0, INF)
-	
+	print(ocean)
 	apply_central_impulse(Vector3(0, buoyancyForce, 0))
 	
-	print(buoyancyForce)
+	var vec1 = Vector3(xP, d1.y, zP)
+	var vec2 = Vector3(xP+0.01, d2.y, zP)
+	var vec3 = Vector3(xP, d3.y, zP+0.01)
+	
+	var dir1 = vec2 - vec1
+	var dir2 = vec3 - vec1
 
+	var norm = dir2.cross(dir1)
+	norm = norm.normalized()
+#
+	var localUp = global_transform.basis.y
+	var correctingMoment = localUp.cross(norm)
+	apply_torque_impulse(correctingMoment)
 
 					
 func _get_movement():
